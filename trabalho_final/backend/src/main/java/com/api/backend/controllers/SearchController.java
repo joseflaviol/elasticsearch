b45667ebe4b531d36181f7a17f9575f9913be695
José Flávio <joseflaviol119@gmail.com>
2022-08-29 00:39:09 -0300

    god save my soul

--- trabalho_final/backend/src/main/java/com/api/backend/controllers/SearchController.java
+++ trabalho_final/backend/src/main/java/com/api/backend/controllers/SearchController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import javax.swing.text.html.Option;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -24,8 +27,8 @@ public class SearchController {
     }
 
     @GetMapping("")
-    public Result search(@RequestParam(name = "must") String must, @RequestParam(name = "must_not", defaultValue = "") String must_not, @RequestParam(name = "should", defaultValue = "") String should, @RequestParam(name = "page", defaultValue = "1") int page, @RequestParam(name = "page_size", defaultValue = "10") int page_size) throws IOException {
-        return this.searchService.search(must, must_not, should, page, page_size);
+    public Result search(@RequestParam(name = "must") String must, @RequestParam(name = "must_field", defaultValue = "content") String must_field, @RequestParam(name = "must_match_phrase") Optional<Boolean> must_match_phrase, @RequestParam(name = "must_not", defaultValue = "") String must_not, @RequestParam(name = "must_not_field", defaultValue = "content") String must_not_field, @RequestParam(name = "must_not_match_phrase") Optional<Boolean> must_not_match_phrase, @RequestParam(name = "should", defaultValue = "") String should, @RequestParam(name = "should_field", defaultValue = "content") String should_field, @RequestParam(name = "should_match_phrase") Optional<Boolean> should_match_phrase, @RequestParam(name = "page", defaultValue = "1") int page, @RequestParam(name = "page_size", defaultValue = "10") int page_size) throws IOException {
+        return this.searchService.search(must, must_field, must_match_phrase.orElse(false), must_not, must_not_field, must_not_match_phrase.orElse(false), should, should_field, should_match_phrase.orElse(false), page, page_size);
     }
 
 }
--- trabalho_final/backend/src/main/java/com/api/backend/services/SearchService.java
+++ trabalho_final/backend/src/main/java/com/api/backend/services/SearchService.java
@@ -4,6 +4,8 @@ import com.api.backend.models.Document;
 import com.api.backend.models.Result;
 import com.api.backend.models.Search;
 import org.opensearch.client.opensearch.OpenSearchClient;
+import org.opensearch.client.opensearch._types.query_dsl.BoolQuery;
+import org.opensearch.client.opensearch._types.query_dsl.Query;
 import org.opensearch.client.opensearch.core.SearchResponse;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,7 @@ import java.util.stream.Collectors;
 @Service
 public class SearchService {
 
-    public Result search(String must, String must_not, String should, int page, int page_size) throws IOException {
+    public Result search(String must, String must_field, boolean must_match_phrase, String must_not, String must_not_field, boolean must_not_match_phrase, String should, String should_field, boolean should_match_phrase, int page, int page_size) throws IOException {
         Search search = new Search("localhost", 9200);
 
         OpenSearchClient client = search.osClient();
@@ -23,29 +25,50 @@ public class SearchService {
                         .index("wikipedia")
                         .query(q -> q
                                 .bool(b -> b
-                                        .must(query -> query
-                                                .match(m -> m
+                                        .must(query -> {
+                                                if (must_match_phrase) {
+                                                    return query.matchPhrase(mf -> mf
+                                                            .query(must)
+                                                            .field(must_field)
+                                                    );
+                                                }
+                                                return query.match(m -> m
                                                         .query(matchQuery -> matchQuery
                                                                 .stringValue(must)
                                                         )
-                                                        .field("content")
-                                                )
+                                                        .field(must_field)
+                                                );
+                                            }
                                         )
-                                        .mustNot(query -> query
-                                                .match(m -> m
-                                                        .query(matchQuery -> matchQuery
-                                                                .stringValue(must_not)
-                                                        )
-                                                        .field("content")
-                                                )
+                                        .mustNot(query -> {
+                                                    if (must_not_match_phrase) {
+                                                        return query.matchPhrase(mf -> mf
+                                                                .query(must_not)
+                                                                .field(must_not_field)
+                                                        );
+                                                    }
+                                                    return query.match(m -> m
+                                                            .query(matchQuery -> matchQuery
+                                                                    .stringValue(must_not)
+                                                            )
+                                                            .field(must_not_field)
+                                                    );
+                                                }
                                         )
-                                        .should(query -> query
-                                                .match(m -> m
-                                                        .query(matchQuery -> matchQuery
-                                                                .stringValue(should)
-                                                        )
-                                                        .field("content")
-                                                )
+                                        .should(query -> {
+                                                    if (should_match_phrase) {
+                                                        return query.matchPhrase(mf -> mf
+                                                                .query(should)
+                                                                .field(should_field)
+                                                        );
+                                                    }
+                                                    return query.match(m -> m
+                                                            .query(matchQuery -> matchQuery
+                                                                    .stringValue(should)
+                                                            )
+                                                            .field(should_field)
+                                                    );
+                                                }
                                         )
                                 )
                         )
